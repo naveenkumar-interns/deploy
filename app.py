832acea8652359a41541f73182af4b0d3005c6a9
naveenkumar-interns <naveenkumar.intern@gmail.com>
2025-02-04 12:47:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ load_dotenv()
 
 app = Flask(__name__)
 llm = ChatGoogleGenerativeAI(
-model="gemini-1.5-pro",
-temperature=0,
-max_tokens=None,
-timeout=None,
-max_retries=2,
-google_api_key=os.getenv("GOOGLE_API_KEY"),
-)
+    model="gemini-1.5-pro",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    )
 @app.route('/hello', methods=['GET'])
 def hello():
     return jsonify({'message': 'Hello, World!'})
