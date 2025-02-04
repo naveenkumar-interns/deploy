6f39acd231c9cf8c5e4230739c7a51a6381a896e
naveenkumar-interns <naveenkumar.intern@gmail.com>
2025-02-04 12:46:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, request, jsonify
 from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
 import os
+load_dotenv()
 
 app = Flask(__name__)
-
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-pro",
-    temperature=0.7,
-    max_tokens=50,
-    timeout=None,
-    max_retries=2,
-    google_api_key=os.getenv("GOOGLE_API_KEY"),
+model="gemini-1.5-pro",
+temperature=0,
+max_tokens=None,
+timeout=None,
+max_retries=2,
+google_api_key=os.getenv("GOOGLE_API_KEY"),
 )
-
 @app.route('/hello', methods=['GET'])
 def hello():
     return jsonify({'message': 'Hello, World!'})
@@ -22,3 +22,6 @@ def hello():
 def llm():
     text = llm.invoke("hi").content
     return jsonify({'message': 'Hello, World!',"response":text})
+
+if __name__ == '__main__':
+    app.run()
