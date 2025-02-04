a3837b80d3498aa674a94601a1fd72c6490129e0
naveenkumar-interns <naveenkumar.intern@gmail.com>
2025-02-04 12:12:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,21 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-pro",
+    temperature=0.7,
+    max_tokens=50,
+    timeout=None,
+    max_retries=2,
+    google_api_key=os.getenv("GOOGLE_API_KEY"),
+)
+
 @app.route('/hello', methods=['GET'])
 def hello():
     return jsonify({'message': 'Hello, World!'})
+
+
+@app.route('/llm', methods=['GET'])
+def llm():
+    text = llm.invoke("hi")
+    return jsonify({'message': 'Hello, World!',"response":text})
